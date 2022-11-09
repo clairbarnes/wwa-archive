53aaf339ddbaaf9a2888a09c445ed60c5425a16b
clairbarnes <clair.with.no.e@gmail.com>
2022-11-09 18:41:46 +0000

    Attribution script for Lower Niger flooding

--- 11_climexp/climexp_attribution_0.sh
+++ 11_climexp/climexp_attribution_0.sh
@@ -1,15 +1,21 @@
 
-model_list=`echo p_lake-chad_jjas-mean_IPSL-CM5A-LR_r1_REMO2009:tas_Amon_IPSL-CM5A-LR_rcp85_r1i1p1 p_lake-chad_jjas-mean_MPI-ESM-LR_r1_REMO2009:tas_Amon_MPI-ESM-LR_rcp85_r1i1p1 p_lake-chad_jjas-mean_MPI-ESM-MR_r1_RegCM4-3:tas_Amon_MPI-ESM-MR_rcp85_r1i1p1 p_lake-chad_jjas-mean_HadGEM2-ES_r1_RegCM4-3:tas_Amon_HadGEM2-ES_rcp85_r1i1p1 p_lake-chad_jjas-mean_HadGEM2-ES_r1_REMO2015:tas_Amon_HadGEM2-ES_rcp85_r1i1p1 p__lake-chad_jjas-meanHadGEM2-ES_r1_RegCM4-7:tas_Amon_HadGEM2-ES_rcp85_r1i1p1 p_lake-chad_jjas-mean_MIROC5_r1_REMO2009:tas_Amon_MIROC5_rcp85_r1i1p1 p_lake-chad_jjas-mean_HadGEM2-ES_r1_REMO2009:tas_Amon_HadGEM2-ES_rcp85_r1i1p1 p_lake-chad_jjas-mean_EC-EARTH_r12_REMO2009:tas_Amon_EC-EARTH_rcp85_r12i1p1 p_lake-chad_jjas-mean_NorESM1-M_r1_RegCM4-7:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lake-chad_jjas-mean_MPI-ESM-MR_r1_RegCM4-7:tas_Amon_MPI-ESM-MR_rcp85_r1i1p1`
+model_list=`echo p_lower-niger_7daymax_NorESM1-M_r1_RegCM4-7:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_EC-EARTH_r12_REMO2009:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_HadGEM2-ES_r1_RegCM4-3:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_NorESM1-M_r1_CCLM5-0-15:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_MPI-ESM-LR_r1_REMO2009:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_MPI-ESM-MR_r1_RegCM4-7:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_MPI-ESM-MR_r1_RegCM4-3:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_IPSL-CM5A-LR_r1_REMO2009:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_HadGEM2-ES_r1_REMO2009:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_HadGEM2-ES_r1_REMO2015:tas_Amon_NorESM1-M_rcp85_r1i1p1 p_lower-niger_7daymax_MIROC5_r1_REMO2009:tas_Amon_NorESM1-M_rcp85_r1i1p1`
 
-results_file=results_lake-chad.txt
-attr_script=climexp_attribution.sh
+results_file=results_lower-niger.txt
+attr_script=climexp_attribution_lower-niger.sh
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-## DOWNLOAD LIST OF AVAILABLE DATA
+
+##############################################################################################################################
+####                                  SHOULDN'T NEED TO CHANGE ANYTHING AFTER THIS LINE                                   ####
+##############################################################################################################################
+
+
+## DOWNLOAD LIST OF AVAILABLE DATA ON CLIMATE EXPLORER
 
 curl https://climexp.knmi.nl/userseries.cgi?id=62f4b5a82fde776a4c64f0ca33646aa0 > climexp_uploads.txt
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 ## CALL CLIMATE EXPLORER TO RUN ATTRIBUTION FOR EACH MODEL IN TURN
 
 # loop over the models and run the attribution
@@ -23,10 +29,11 @@ do
 done
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 ## CONCATENATE HEADERS & RESULTS INTO A SINGLE FILE
 
 cat tmp_headers.txt > $results_file
 cat tmp_results.txt >> $results_file
 
 # remove temporary files
-rm tmp_headers.txt tmp_results.txt model_eval.log model_attr.log model_proj.log climexp_uploads.txt
+rm tmp_headers.txt tmp_results.txt model_eval.log model_attr.log model_proj.log

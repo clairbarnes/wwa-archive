9f183fd0d2a73eb323740d0257fd2e7ca8ce0551
clairbarnes <clair.with.no.e@gmail.com>
2023-01-02 17:47:55 +0000

    R functions for extRemes package

--- methods_extRemes.r
+++ methods_extRemes.r
@@ -1,6 +1,7 @@
 suppressMessages({
     library(extRemes)
     library(plyr) 
+    library(beepr)
 })
 
 #=======================================================================================================================
@@ -22,6 +23,7 @@ event_qcov <- function(mdl, covariate) {
 model_pars <- function(mdl) {
     
     if(mdl$type == 'GEV_fixeddisp') {
+        
         return(mdl$results$par)
     } else {
         par_est = ci(mdl, 0.05, "parameter")[,2]
@@ -35,8 +37,12 @@ model_pars <- function(mdl) {
         }
         
         if(!"sigma1" %in% names(par_est)) {
-            
             if("phi1" %in% names(par_est)) { par_est["sigma1"] = (par_est["phi1"]) } else { par_est["sigma1"] = NA }
+        }
+        
+        if(!"mu0" %in% names(par_est)) {
+            par_est["mu0"] <- unname(par_est["location"])
+            par_est["mu1"] <- NA
         }
         return(par_est[c("mu0", "mu1", "sigma0", "sigma1", "shape")])
     } 
@@ -49,17 +55,21 @@ sgev_pars <- function(mdl, covariate, burn.in = 499) {
     
     if(mdl$type == 'GEV_fixeddisp') {
         pars <- mdl$results$par
-        sf <- exp(pars["alpha"] * covariate / pars["mu"])   # scaling factor
-        loc = pars["mu"] * sf
-        scale = pars["sigma"] * sf
-        shape = pars["xi"]
+        sf <- exp(pars["alpha"] * covariate / pars["mu0"])   # scaling factor
+        loc = pars["mu0"] * sf
+        scale = pars["sigma0"] * sf
+        shape = pars["shape"]
     } else {
         
         pars <- strip(mdl, burn.in = burn.in)
         cov <- event_qcov(mdl, covariate)[1:length(pars)]
         cpars <- pars * cov
         
-        loc <- sum(cpars[grep("mu", names(cpars))])
+        if("location" %in% names(cpars)) {
+            loc <- cpars["location"]
+        } else {
+            loc <- sum(cpars[grep("mu", names(cpars))])
+        }
         
         if(any(grepl("phi", names(cpars)))) {
             scale <- exp(sum(cpars[grep("phi", names(cpars))]))
@@ -73,7 +83,7 @@ sgev_pars <- function(mdl, covariate, burn.in = 499) {
         shape <- sum(cpars[grep("shape", names(cpars))])
     }
         
-    return(list("loc" = loc, "scale" = scale, "shape" = shape))
+    return(list("loc" = unname(loc), "scale" = unname(scale), "shape" = unname(shape)))
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -105,12 +115,17 @@ rl.boot.ci <- function(mdl, x, covariate_value, ci = 95, nsamp = 5000, seed = 1)
         
         # resample the source data, run the model again using the bootstrapped data
         boot_df <- mdl$cov.data[sample(1:n,n,replace = T),]
-        boot_fit <- suppressWarnings(update(mdl, data = boot_df))
+        
+        if(mdl_fd$type == "GEV_fixeddisp") {
+            boot_fit <- fevd_fixeddisp("precip", "gmst", data = boot_df)
+        } else {
+            boot_fit <- suppressWarnings(update(mdl, data = boot_df))
+        }
         
         # currently ignoring warnings, but should handle them properly at some point
         # provided methods to calculate return level can be slow, esp for Bayesian 
-        return_level(boot_fit, x, covariate_value)
-                        
+        tryCatch(return_level(boot_fit, x, event_gmst), return(NA))
+          
         # possible alternative method - although this seems even slower
         # pars = lapply(findpars(boot_fit, qcov = event_qcov(boot_fit, covariate_value)), "sum")
         # rl = rlevd(x, loc = pars$location, scale = pars$scale, shape = pars$shape)
@@ -145,21 +160,49 @@ prob_ratio <- function(mdl, value, cov1, cov2) {
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Nonstationary GEV with fixed dispersion (as fitted by climate explorer)
 
-gev_fixeddisp <- function(pars = c(mu, sig, xi, alpha), covariate, x) {
+gev_fixeddisp <- function(pars = c(mu0, sigma0, shape, alpha), covariate, x) {
     
-    loc = pars["mu"] * exp(pars["alpha"] * covariate / pars["mu"])
-    scale = pars["sigma"] * exp(pars["alpha"] * covariate / pars["mu"])
-    shape = pars["xi"]
+    loc = pars["mu0"] * exp(pars["alpha"] * covariate / pars["mu0"])
+    scale = pars["sigma0"] * exp(pars["alpha"] * covariate / pars["mu0"])
+    shape = pars["shape"]
     
     # return negative log-likelihood to be minimised
     return(-sum(devd(x, loc = loc, scale = scale, shape = shape, log = T)))
 }
 
 fevd_fixeddisp <- function(x, covariate, data, method = "MLE", ...) {
-    res <- list("results" = optim(par = c(mu = mean(data[,x]), sigma = sd(data[,x]), xi = 0, alpha = 0), gev_fixeddisp, covariate = data[,covariate], x = data[,x]))
+    
+    res <- list("results" = optim(par = c(mu0 = mean(data[,x]), sigma0 = sd(data[,x]), shape = 0, alpha = 0), gev_fixeddisp, 
+                                  covariate = data[,covariate], x = data[,x]))
     res[["type"]] <- "GEV_fixeddisp"
     res[["x"]] <- data[,x]
     res[["cov.data"]] <- data
+    res[["cov.name"]] <- covariate
     
     return(res)
+}
+
+# location, shape & scale parameters for fixed-dispersion model
+fd_lss <- function(mdl, covariate = NA) {
+    
+    cov_list <- mdl_fd$cov.data[,mdl_fd$cov.name]
+    
+    pars <- mdl$results$par
+    sf <- exp(pars["alpha"] * cov_list / pars["mu0"])   # scaling factor
+    loc = pars["mu0"] * sf
+    scale = pars["sigma0"] * sf
+    shape = pars["shape"]
+    
+    if(is.na(covariate)) {
+        return(list(loc = loc, scale = scale, shape = unname(shape)))
+    } else {
+        i <- which.min(abs(cov_list - covariate))
+        return(list(loc = loc[i], scale = scale[i], shape = unname(shape)))
+    }
+}
+
+trans_fd <- function(mdl) {
+    
+    pars <- fd_lss(mdl)
+    return(log(1 + (mdl_fd$x - pars$loc) * pars$shape / pars$scale) / pars$shape)
 }
